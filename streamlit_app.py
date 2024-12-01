2625055b87813fe39b1f2e444396761d96d0d2b0
Erik Garcia <erik.martaneva@gmail.com>
2024-12-01 10:17:21 -0300

    Layoutização pagina de login

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,19 @@
 #Importações
 import streamlit as st
 import time
+
+#Formulário de Login
+with st.form("login_form"):
+    st.write("Login")
+    st.divider()
+    
+    st.text_input("Nome de usuario:")
+    st.text_input("Senha")
+    st.divider()
+    
+    st.form_submit_button(
+        label = "Entrar",
+        help = "Precisa de ajuda?",
+        on_click = None,
+        type = "primary"
+    )
