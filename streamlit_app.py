e1328d611a17af20994bd341322dee45f4f238d5
Erik Garcia <erik.martaneva@gmail.com>
2024-12-01 10:36:43 -0300

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 #Pagina de Login
 
+#Configurações principais
+st.set_page_config(
+    page_title = "Frangoeste - Contorle de Descartes",
+    page_icon = "",
+    layout = "wide",
+)
+
+
 #Importações
 import streamlit as st
 import time
@@ -19,3 +27,7 @@ with st.form("login_form"):
         on_click = None,
         type = "primary"
     )
+
+pagina1 = st.page(str = "home", path = "paginas/home.py")
+pagina2 = st.page(str = "configuração", path = "paginas/configuracoes.py")
+pagina3 = st.page(str = "relatorios", path = "paginas/relatorios.py")
