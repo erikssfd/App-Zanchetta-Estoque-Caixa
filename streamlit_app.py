98086796a6a241b700f695fbc828acfca3e5c8b5
Erik Garcia <erik.martaneva@gmail.com>
2024-12-01 09:24:13 -0300

    Adicionando layout(Menu lateral e importando time)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+#Importando Bibliotecas
 import streamlit as st
+import time
 
+#Definindo variáveis e menu lateral
+
+#Titulo e logotipo lateral
 st.title("Frangoeste - Controle de descartes")
+
+#Menu lateral
+with st.sidebar:
+    st.write("Menu Lateral")
