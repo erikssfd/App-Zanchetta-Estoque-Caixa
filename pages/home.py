95bb63d9b83139c3dc887029b31f1c0b99f8c01b
Erik Garcia <erik.martaneva@gmail.com>
2024-12-22 03:15:58 +0000

    ss

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit_app
 
 def home():
-    st.markdown("*No posso te ajudar hoje? *" + st.session_state.username)
+    st.markdown("*No posso te ajudar hoje? *" + st.session_state.user_name)
 
     st.markdown(
         "Possuo algumas opções bem interessantes que possa lhe interessar."
